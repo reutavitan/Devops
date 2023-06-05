ddc3ffb489a69dbf9dfc7ac4fe5475eb00be8652
reutavitan <73298850+reutavitan@users.noreply.github.com>
2023-06-05 21:23:02 +0300

    Update test1.test.js
    200

--- test/test1.test.js
+++ test/test1.test.js
@@ -15,7 +15,7 @@ app.use('/', router);
 describe('Test suite 1:', () => {
   test('test 1: ', async () => {
     const res = await request(app).get('/');
-    expect(res.statusCode).toEqual(500);
+    expect(res.statusCode).toEqual(200);
   });
 
   test('test 2: ', async () => {
