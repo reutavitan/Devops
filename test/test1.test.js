f3b785fcc82460e2b5b355d61a7f0b01d674579d
reutavitan <73298850+reutavitan@users.noreply.github.com>
2023-06-05 21:21:36 +0300

    Update test1.test.js
    done

--- test/test1.test.js
+++ test/test1.test.js
@@ -1,69 +1,79 @@
-const request = require('supertest')
-const express = require('express')
-const mongoose = require('mongoose')
-const User = require('../models/user')
-
-const app = express()
+const request = require('supertest');
+const express = require('express');
+const mongoose = require('mongoose');
+var User = require('../models/user');
+const app = express();
+var  error = 500;
+var Success= 500;
 
 // Import the router module
-const router = require('../routes/index')
+const router = require('../routes/index');
 
 // Mount the router in the Express app
-app.use('/', router)
+app.use('/', router);
 
 describe('Test suite 1:', () => {
   test('test 1: ', async () => {
-    const res = await request(app).get('/')
-    expect(res.statusCode).toEqual(200)
-  })
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(500);
+  });
 
   test('test 2: ', async () => {
-    const res = await request(app).get('/1234')
-    expect(res.statusCode).toEqual(404)
-  })
-})
+    const res = await request(app).get('/1234');
+    expect(res.statusCode).toEqual(404);
+  });
+});
 
-describe('Test suite 3:', () => {
+describe('Test suite 2:', () => {
   beforeAll(() => {
-    jest.setTimeout(30000) // Set the timeout value to 30000ms (30 seconds) for the entire suite
-  })
+    jest.setTimeout(30000); // Set the timeout value to 30000ms (30 seconds) for the entire suite
+  });
+
   test('test 3: Register user', async () => {
-    const userData = {
+    var userData = {
       email: 'test1@example.com',
       username: 'testuser1',
-      password: 'password1234',
-      passwordConf: 'password1234',
       exam1: 99,
       exam2: 90,
-      exam3: 95
-    }
+      exam3: 95,
+      password: 'password1234',
+      passwordConf: 'password1234',
+    };
+    await request(app).post('/').send(userData).expect(Success);
+  });
 
-    const res = await request(app).post('/').send(userData)
-    expect(res.statusCode).toEqual(500)
-  })
+  test('test 3: Register user Erroneous', async () => {
+    var userData = {
+      email: 'test1@example.com',
+      username: 'testuser1',
+      exam1: 999,
+      exam2: 90,
+      exam3: 95,
+      password: 'password1234',
+      passwordConf: 'password12345',
+    };
+    await request(app).post('/').send(userData).expect(error);
+  });
 
   test('test 4: Login user', async () => {
-    const loginData = {
-      email: 'test@example.com',
-      password: 'password123'
-    }
-
-    const res = await request(app)
+    await request(app)
       .post('/login')
-      .send(loginData)
+      .send({ email: 'test@example.com', password: 'password123' })
+      .expect(Success);
+  });
 
-    expect(res.statusCode).toEqual(500)
-  })
+  test('test 4: Login user Erroneous', async () => {
+    await request(app)
+      .post('/login')
+      .send({ email: 'test@example.com', password: 'password' })
+      .expect( error);
+  });
 
   test('test 5: Access profile', async () => {
-    const agent = request.agent(app)
+    const res = await request(app).get('/profile').expect(Success);
+    // Perform any assertions on res as needed
+  });
 
-    // Login the user
-    await agent.post('/login').send({ email: 'test@example.com', password: 'password123' })
+   
+});
 
-    // Access the profile route
-    const res = await agent.get('/profile')
-
-    expect(res.statusCode).toEqual(500)
-  })
-})
